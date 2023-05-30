8b112c43943f5a547574da2526264763399089d2
MadelineSey <seyrambadasu@gmail.com>
2023-05-30 16:42:19 -0700

    handle print

--- handler_print.c
+++ handler_print.c
@@ -19,14 +19,14 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_nonPrintable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
 	for (m = 0; fmt_types[m].fmt != '\0'; m++)
 		if (fmt[*ind] == fmt_types[m].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+			return (fmt_types[m].fn(list, buffer, flags, width, precision, size));
 
-	if (fmt_types[i].fmt == '\0')
+	if (fmt_types[m].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
